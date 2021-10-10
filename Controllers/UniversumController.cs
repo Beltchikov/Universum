b56d2fadd829eb1a68aa8223eab3b1cb82aa8674
Jogger <beltchikov@gmx.de>
2021-10-10 17:27:27 +0200

    Adds XPath logic

--- Controllers/UniversumController.cs
+++ Controllers/UniversumController.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Universum.Controllers
@@ -12,14 +13,14 @@ namespace Universum.Controllers
         public async Task<ActionResult> CurrentPrice(string symbol)
         {
             string url = @$"https://finance.yahoo.com/quote/{symbol}";
+            string xPath = "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div[1]/span[1]";
 
             HtmlWeb htmlWeb = new HtmlWeb();
             HtmlDocument htmlDocument = await Task.Factory.StartNew(() => htmlWeb.Load(url));
-            //var nodes = htmlDocument.DocumentNode.SelectNodes(xPath);
-            //return nodes.Select(n => n.InnerText);
-
-            var result = htmlDocument.DocumentNode.InnerText;
-            return Json(new { Content= result });
+            var nodes = htmlDocument.DocumentNode.SelectNodes(xPath);
+            var result = nodes.Select(n => n.InnerText);
+            
+            return Json(result);
         }
 
     }
