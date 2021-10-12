2010887968d5538f0f6c41d7d2f2933ed365a2e5
Andrei Beltchikov <beltchikov@gmx.de>
2021-10-12 16:40:37 +0200

    Implements Roe method

--- Controllers/UniversumController.cs
+++ Controllers/UniversumController.cs
@@ -23,5 +23,18 @@ namespace Universum.Controllers
             return Json(result);
         }
 
+        [HttpGet]
+        public async Task<ActionResult> Roe(string symbol)
+        {
+            string url = @$"https://finance.yahoo.com/quote/{symbol}/key-statistics";
+            string xPath = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[3]/div/div[3]/div/div/table/tbody/tr[2]/td[2]";
+
+            HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlDocument htmlDocument = await Task.Factory.StartNew(() => htmlWeb.Load(url));
+            var nodes = htmlDocument.DocumentNode.SelectNodes(xPath);
+            var result = nodes.Select(n => n.InnerText);
+
+            return Json(result);
+        }
     }
 }
