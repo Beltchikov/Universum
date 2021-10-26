2a4e452f2f4e9c5c25819d63b553abdce76796cb
Andrei Beltchikov <beltchikov@gmx.de>
2021-10-26 16:10:09 +0200

    Minor improvements

--- Controllers/UniversumController.cs
+++ Controllers/UniversumController.cs
@@ -14,11 +14,11 @@ namespace Universum.Controllers
         [HttpGet]
         public async Task<ActionResult> CurrentPrice(string symbol)
         {
-            string url = @$"https://finance.yahoo.com/quote/{symbol}";
-            string xPath = "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div[1]/span[1]";
+            var url = @$"https://finance.yahoo.com/quote/{symbol}";
+            var xPath = "/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div[1]/span[1]";
 
-            HtmlWeb htmlWeb = new HtmlWeb();
-            HtmlDocument htmlDocument = await Task.Factory.StartNew(() => htmlWeb.Load(url));
+            var htmlWeb = new HtmlWeb();
+            var htmlDocument = await Task.Factory.StartNew(() => htmlWeb.Load(url));
             var nodes = htmlDocument.DocumentNode.SelectNodes(xPath);
             var result = nodes.Select(n => n.InnerText);
             
@@ -28,11 +28,11 @@ namespace Universum.Controllers
         [HttpGet]
         public async Task<ActionResult> Roe(string symbol)
         {
-            string url = @$"https://finance.yahoo.com/quote/{symbol}/key-statistics";
-            string xPath = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[3]/div/div[3]/div/div/table/tbody/tr[2]/td[2]";
+            var url = @$"https://finance.yahoo.com/quote/{symbol}/key-statistics";
+            var xPath = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div[2]/div[3]/div/div[3]/div/div/table/tbody/tr[2]/td[2]";
 
-            HtmlWeb htmlWeb = new HtmlWeb();
-            HtmlDocument htmlDocument = await Task.Factory.StartNew(() => htmlWeb.Load(url));
+            var htmlWeb = new HtmlWeb();
+            var htmlDocument = await Task.Factory.StartNew(() => htmlWeb.Load(url));
             var nodes = htmlDocument.DocumentNode.SelectNodes(xPath);
             var result = nodes.Select(n => n.InnerText);
 
@@ -42,9 +42,9 @@ namespace Universum.Controllers
         [HttpGet]
         public ActionResult TargetPrice(string symbol)
         {
-            string url = @$"https://finance.yahoo.com/quote/{symbol}/analysis?p={symbol}";
-            string pattern = @"targetMeanPrice(\""|:|{|\w|\.)*";
-            string pattern2 = @"\d+\.+\d+";
+            var url = @$"https://finance.yahoo.com/quote/{symbol}/analysis?p={symbol}";
+            var pattern = @"targetMeanPrice(\""|:|{|\w|\.)*";
+            var pattern2 = @"\d+\.+\d+";
 
             // https://github.com/SimpleBrowserDotNet/SimpleBrowser
             var browser = new Browser();
