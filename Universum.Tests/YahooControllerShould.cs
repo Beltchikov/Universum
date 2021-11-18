046756d1edc0a034b99a1e5143c071e2d5ad21b3
Andrei Beltchikov <beltchikov@gmx.de>
2021-11-18 15:05:12 +0100

    Adds unit tests

--- Universum.Tests/YahooControllerShould.cs
+++ Universum.Tests/YahooControllerShould.cs
@@ -29,5 +29,46 @@ namespace Universum.Tests
 
             Assert.Equal(currentPriceExpected, currentPrice);
         }
+
+        [Fact]
+        public void ReturnRoe()
+        {
+            double roeExpected = 32;
+            var roeReceived= "0.324";
+
+            var simpleBrowser = Substitute.For<ISimpleBrowser>();
+            simpleBrowser.OneValueResult(
+                    Arg.Any<string>(),
+                    Arg.Any<string>(),
+                    Arg.Any<string>())
+                .Returns(roeReceived);
+
+            var sut = new YahooController(simpleBrowser);
+            JsonResult jsonResult = (JsonResult)sut.Roe("MSFT");
+            double[] roeArray = (double[])jsonResult.Value;
+            var roe = roeArray[0];
+
+            Assert.Equal(roeExpected, roe);
+        }
+
+        [Fact]
+        public void ReturnTargetPrice()
+        {
+            string targetPriceExpected = "45.78";
+
+            var simpleBrowser = Substitute.For<ISimpleBrowser>();
+            simpleBrowser.OneValueResult(
+                    Arg.Any<string>(),
+                    Arg.Any<string>(),
+                    Arg.Any<string>())
+                .Returns(targetPriceExpected);
+
+            var sut = new YahooController(simpleBrowser);
+            JsonResult jsonResult = (JsonResult)sut.CurrentPrice("MSFT");
+            string[] targetPriceArray = (string[])jsonResult.Value;
+            var targetPrice = targetPriceArray[0];
+
+            Assert.Equal(targetPriceExpected, targetPrice);
+        }
     }
 }
